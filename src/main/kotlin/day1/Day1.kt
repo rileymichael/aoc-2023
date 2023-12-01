9a22ff8e5471ef25d487bfb9bae725499d6531a6
RileyMichael <riley.mcgee.13@gmail.com>
2023-11-30 23:38:57 -0600

    solve day 1

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -3,12 +3,34 @@ package com.github.rileymichael.day1
 import com.github.rileymichael.Puzzle
 
 
-object Day1 : Puzzle<Sequence<Int>>(1) {
-    override fun parse(input: Sequence<String>): Sequence<Int> = TODO()
+object Day1 : Puzzle<Sequence<String>>(1) {
+    override fun parse(input: Sequence<String>): Sequence<String> = input
 
-    override fun part1(input: Sequence<Int>) = TODO()
+    override fun part1(input: Sequence<String>) = input.sumOf { line ->
+        val ints = line.filter { char -> char.isDigit() }
+        "${ints.first()}${ints.last()}".toInt()
+    }
 
-    override fun part2(input: Sequence<Int>) = TODO()
+    override fun part2(input: Sequence<String>) = input.sumOf { line ->
+        var newLine = line
+        replacements.forEach { (match, replacement) ->
+            newLine = newLine.replace(match, replacement)
+        }
+        val ints = newLine.filter { char -> char.isDigit() }
+        "${ints.first()}${ints.last()}".toInt()
+    }
 }
+
+private val replacements = mapOf(
+    "one" to "o1e",
+    "two" to "t2o",
+    "three" to "t3e",
+    "four" to "f4r",
+    "five" to "f5e",
+    "six" to "s6x",
+    "seven" to "s7n",
+    "eight" to "e8t",
+    "nine" to "n9e",
+)
 
 
--- src/test/kotlin/day1/Day1Test.kt
+++ src/test/kotlin/day1/Day1Test.kt
@@ -4,22 +4,36 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class Day1Test : StringSpec({
-    val input = """
-    """
+    val part1Input = """
+1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet
+    """.trimIndent()
+
+    val part2Input = """
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+    """.trimIndent()
 
     "part 1 example" {
-        Day1.solve(Day1::part1, input) shouldBe TODO()
+        Day1.solve(Day1::part1, part1Input) shouldBe 142
     }
 
     "part 1 solution" {
-        Day1.solve(Day1::part1) shouldBe TODO()
+        Day1.solve(Day1::part1) shouldBe 53334
     }
 
     "part 2 example" {
-        Day1.solve(Day1::part2, input) shouldBe TODO()
+        Day1.solve(Day1::part2, part2Input) shouldBe 281
     }
 
     "part 2 solution" {
-        Day1.solve(Day1::part2) shouldBe TODO()
+        Day1.solve(Day1::part2) shouldBe 52834
     }
 })
